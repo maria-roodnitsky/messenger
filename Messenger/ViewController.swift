5289967a5d406ae43e4aee7069eb36186bc83874
Maria Roodnitsky <maria.roodnitsky@gmail.com>
2021-09-21 11:58:36 -0700

    first commit, starter code with github on this computer

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Maria Roodnitsky on 9/21/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
